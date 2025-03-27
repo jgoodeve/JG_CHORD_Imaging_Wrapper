a6c310e971a4ade560cbcd278fa69e4c9b61e299
hanshopkins <hshopkin@uwaterloo.ca>
2025-03-27 14:11:52 -0400

    Changed gif minimum and colorbar option

--- dirtymap_gifmaker.py
+++ dirtymap_gifmaker.py
@@ -11,6 +11,10 @@ if len(sys.argv) == 1:
 else:
 	filename = sys.argv[1]
 
+colorbar = False
+if len(sys.argv) == 2 and sys.argv[1] == "colorbar":
+    colorbar = True
+
 dm_file = open(filename,"rb")
 dm_dict = pickle.load(dm_file)
 dm = dm_dict["dirtymap"]
@@ -21,13 +25,15 @@ ny = dm_dict["ny"]
 
 dm = dm.reshape([ny,nx,nframes])
 maxInArray = np.max(dm)
+minInArray = np.min(dm)
 
 if not os.path.exists("/tmp/animate"):
     os.mkdir("/tmp/animate")
 print("Plotting...")
 for i in range(nframes):
-    plt.imshow(dm[:,:,i], vmin=0, vmax=maxInArray, interpolation="none", origin="lower")
+    plt.imshow(dm[:,:,i], vmin=minInArray, vmax=maxInArray, interpolation="none", origin="lower")
     plt.title(f"Freq: {frequencies[i]:.4f} MHz")
+    if colorbar: plt.colorbar()
     plt.savefig("/tmp/animate/dm"+str(i)+".png", bbox_inches = 'tight', pad_inches = 0.5)
     plt.close()
     

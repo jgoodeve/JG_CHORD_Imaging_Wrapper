eca8bbf7a6232b7cace0c966d1546d4856b701c6
Hans Hopkins <hshopkin@uwaterloo.ca>
2025-04-17 13:58:41 -0700

    added wrapper option for gpu version of the noise simulator

--- dm_noise_simulator/dm_noise_simulator_wrapper.py
+++ dm_noise_simulator/dm_noise_simulator_wrapper.py
@@ -51,3 +51,30 @@ def dm_noise_simulator_instantaneous_wrapper (noise, u, baselines, baseline_coun
                                dec, dish_diameter,
                                noise_map)
     return noise_map
+
+try:
+	gpu_dmns_lib = ctypes.CDLL(os.path.join(os.path.dirname(__file__),"gpu_dmns.so"))
+	cuda_dm_noise_sim_function = gpu_dmns_lib.dm_noise_sim_caller
+	cuda_dm_noise_sim_function.argtypes = \
+    	[ctypes.c_double, ndpointer(dtype=ctypes.c_double), ctypes.c_int,
+    	ndpointer(dtype=ctypes.c_double), ndpointer(dtype=ctypes.c_int), ctypes.c_int,
+    	ndpointer(dtype=ctypes.c_double), ctypes.c_int,
+    	ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_int, 
+    	ndpointer(dtype=ctypes.c_double)]
+
+	def dm_noise_simulator_wrapper_gpu (noise, u, baselines, baseline_counts, wavelengths, dec, dish_diameter, deg_distance_to_count, ntimesamples):
+		assert(baselines.shape[0] == baseline_counts.shape[0])
+		assert(u.shape[1] == 3)
+		assert(baselines.shape[1] == 2)
+		noise_map = np.empty(u.shape[0]*wavelengths.shape[0])
+		u_flattened = u.flatten().copy()
+		gpu_dm_noise_sim_function(noise, u_flattened, u_flattened.shape[0]//3,
+        	                       baselines, baseline_counts, baselines.shape[0],
+        	                       wavelengths, wavelengths.shape[0],
+        	                       dec, dish_diameter, deg_distance_to_count, ntimesamples,
+        	                       noise_map)
+		return noise_map
+except:
+	pass
+
+
